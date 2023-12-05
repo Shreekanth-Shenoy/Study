bc4424562e57c0e145a3d397ded1edbf40d73f7c
Shreekanth-Shenoy <68632078+Shreekanth-Shenoy@users.noreply.github.com>
2023-12-05 12:47:05 +0530

    Update ConsumerProducer.cpp

--- ConsumerProducer.cpp
+++ ConsumerProducer.cpp
@@ -1,50 +1,65 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <deque>
 #include <condition_variable>
 
-class Buffer {
-public:
-    Buffer() : maxBufferSize(50) {}
+using namespace std;
 
-    void produce(int val) {
-        while (val) {
-            std::unique_lock<std::mutex> locker(mu);
-            cond.wait(locker, [this](){return buffer.size() < maxBufferSize;});
-            buffer.push_back(val);
-            std::cout << "Produced: " << val << std::endl;
+
+
+class Buffer{
+public:    
+    Buffer(int size):maxBufferSize(size){}
+    
+    void producer(int val){
+        while(val){
+            std::unique_lock<std::mutex> locker(mu); 
+            cond.wait(locker,[this](){return buffer.size()<maxBufferSize;});
+            buffer.push_back(101-val);
+            cout<<"produced "<<(101-val)<<endl;
             val--;
             locker.unlock();
             cond.notify_one();
-        }
+        }    
     }
-
-    void consume() {
-        while (true) {
+    void consumer(){
+        
+        while(true){
             std::unique_lock<std::mutex> locker(mu);
-            cond.wait(locker, [this](){return buffer.size() > 0;});
-            int val = buffer.back();
-            buffer.pop_back();
-            std::cout << "Consumed: " << val << std::endl;
+            cond.wait(locker,[this](){return buffer.size()>0;});
+            int val = buffer.front();
+            buffer.pop_front();
+            cout<<"Consumed "<<val<<endl;
             locker.unlock();
             cond.notify_one();
         }
     }
-
-private:
-    std::mutex mu;
-    std::condition_variable cond;
-    std::deque<int> buffer;
-    const unsigned int maxBufferSize;
+    
+    
+    private:
+        std::mutex mu;
+        std::condition_variable cond;
+        std::deque<int> buffer;
+        int maxBufferSize;
 };
 
-int main() {
-    Buffer bufferObj;
 
-    std::thread t1(&Buffer::produce, &bufferObj, 100);
-    std::thread t2(&Buffer::consume, &bufferObj);
 
+int main()
+{
+    Buffer bufferObj(10);
+    
+    std::thread t1(&Buffer::producer, &bufferObj,100);
+    std::thread t2(&Buffer::consumer, &bufferObj);
     t1.join();
     t2.join();
 
